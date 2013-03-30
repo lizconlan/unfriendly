cd93c6edb3cc1ef17b585fe0d762ac3d6d6e5ac6
lizconlan <lizconlan@gmail.com>
2013-03-30 17:00:29 +0000

    first attempt at Twitter stuff using application auth / Bearer token

--- lib/twitter_list.rb
+++ lib/twitter_list.rb
@@ -1,15 +1,24 @@
-require 'oauth'
 require 'json'
+require 'yaml'
 require 'rest-client'
+require 'base64'
 
 class TwitterList
-  def initialize()
-    #get the token etc, etc to get OAuth set up (unless that ends up being delegated to something else)
+  def initialize
+    config = YAML::load(File.open './../config/oauth.yml')
+    
+    credentials = Base64.encode64("#{CGI.escape(config[:consumer_key])}:#{CGI.escape(config[:consumer_secret])}")
+    
+    response = RestClient.post 'https://api.twitter.com/oauth2/token', "grant_type=client_credentials", {:authorization => "Basic #{credentials.gsub("\n", "")}", :content_type => "application/x-www-form-urlencoded;charset=UTF-8"}
+    content = JSON.parse(response)
+    @bearer_token = content["access_token"]
   end
   
   def get_list(screen_name)
     #e.g. https://dev.twitter.com/docs/api/1.1/get/friends/ids
-    result = RestClient.get("https://api.twitter.com/1.1/friends/ids.json?screen_name=#{screen_name}")
+    result = RestClient.get "https://api.twitter.com/1.1/friends/ids.json?screen_name=#{screen_name}", {:authorization => "Bearer #{@bearer_token}"}
+    content = JSON.parse(response)
+    content["ids"]
   end
   
   def lookup_ids(screen_name, list)
