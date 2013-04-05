c43dd614410f52b45c5bada94891cede5d3974b3
lizconlan <lizconlan@gmail.com>
2013-04-05 03:30:35 +0100

    getting rid of TwitterList

--- models/user.rb
+++ models/user.rb
@@ -4,11 +4,17 @@ class User
   include Mongoid::Document
   
   field :email, type: String
-  field :password, type: String
+  field :screen_name, type: String
+  field :twitter_id, type: String
+  field :verifier, type: String
   field :friend_ids, type: Array
   field :archived_ids, type: Array #one set only!
   field :last_check_date, type: DateTime
   field :archive_store_date, type: DateTime
+  
+  def twitter_list
+    @twitter_list || set_twitter_list()
+  end
   
   def live_list
     #@twitterlist.get_list()
@@ -21,6 +27,9 @@ class User
   end
   
   private
+    def set_twitter_list
+    end
+    
     def diff
       #diff between live_list and stored_list
       #call out to @twitterlist.lookup_ids?
