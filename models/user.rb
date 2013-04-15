371c78181318a43eaff2fb95803836f270072c5e
lizconlan <lizconlan@gmail.com>
2013-04-15 13:14:41 +0100

    first draft follower lookup wiring

--- models/user.rb
+++ models/user.rb
@@ -1,37 +1,22 @@
 require 'mongoid'
 
 class User
+  include Enumerable
   include Mongoid::Document
+  embeds_many :follower_changes
   
   field :email, type: String
   field :screen_name, type: String
   field :twitter_id, type: String
   field :verifier, type: String
   field :friend_ids, type: Array
-  field :archived_ids, type: Array #one set only!
-  field :last_check_date, type: DateTime
-  field :archive_store_date, type: DateTime
+end
+
+class FollowerChange
+  include Mongoid::Document
+  embedded_in :user
   
-  def twitter_list
-    @twitter_list || set_twitter_list()
-  end
-  
-  def live_list
-    #@twitterlist.get_list()
-  end
-  
-  def unfollowed
-  end
-  
-  def added
-  end
-  
-  private
-    def set_twitter_list
-    end
-    
-    def diff
-      #diff between live_list and stored_list
-      #call out to @twitterlist.lookup_ids?
-    end
+  field :followed, type: Array
+  field :unfollowed, type: Array
+  field :check_date, type: DateTime
 end
--- unfriendly.rb
+++ unfriendly.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sinatra/flash'
 require 'mongoid'
+require 'set'
 
 class Unfriendly < Sinatra::Base
   register Sinatra::Flash
@@ -15,15 +16,25 @@ class Unfriendly < Sinatra::Base
   
   Mongoid.load!("./config/mongo.yml")
   
-  get "/" do
-    if session[:twitter]
-      @twitter = session[:twitter]
-      #raise session[:access_token].params.inspect
-      
-      response = @twitter.get("/1.1/friends/ids.json?screen_name=#{@twitter.screen_name}")
-      raise JSON.parse(response.body).inspect
+  helpers do
+    def format_screen_name_array(arr)
+      output = []
+      arr.each_with_index do |name, index|
+        output << %Q|<a href="http://twitter.com/#{name}">@#{name}</a>|
+        case index
+        when arr.size-1
+          #last item, add no extras
+        when arr.size-2
+          output << " and "
+        else
+          output << ", "
+        end
+      end
+      output.join("")
     end
-      
+  end
+  
+  get "/" do
     haml(:index)
   end
   
@@ -40,7 +51,7 @@ class Unfriendly < Sinatra::Base
     session[:token] = nil
     session[:secret] = nil
     session[:twitter] = @twitter
-    redirect "/"
+    redirect "/check"
   end
   
   get "/logout/?" do
@@ -50,9 +61,77 @@ class Unfriendly < Sinatra::Base
   end
   
   get "/check/?" do
-    redirect "/login" unless session[:twitter]
+    redirect "/twitter_login" unless session[:twitter]
     
-    user = User.where(email: supplied_email).first
-    redirect "/login" unless user
+    @twitter = session[:twitter]
+    @user = User.find_or_create_by(:twitter_id => @twitter.user_id, :screen_name => @twitter.screen_name)
+    
+    response = @twitter.get("/1.1/friends/ids.json?screen_name=#{@twitter.screen_name}")
+    data = JSON.parse(response.body)
+    @current_list = data["ids"]
+    
+    if @user.friend_ids.nil? or @user.friend_ids.empty?
+      #new user, hello!
+      @user.friend_ids = @current_list
+      @user.save!
+    else
+      #welcome back, let's check things
+      unless @user.friend_ids == @current_list
+        change_data = analyse_changes(Set.new(@user.friend_ids), Set.new(@current_list))
+        @followed = change_data[:followed]
+        @unfollowed = change_data[:unfollowed]
+        
+        if @user.follower_changes.empty? or (@followed != @user.follower_changes.last.followed or @unfollowed != @user.follower_changes.last.unfollowed)
+          @user.friend_ids = @current_list
+          @user.update
+          @change = FollowerChange.new
+          @change.followed = @followed
+          @change.unfollowed = @unfollowed
+          @change.check_date = Time.now()
+          @user.follower_changes << @change
+        else
+          @change = @user.follower_changes.last
+          @followed = @change.followed
+          @unfollowed = @change.unfollowed
+        end
+      else
+        @change = @user.follower_changes.last
+        if @change
+          @followed = @change.followed
+          @unfollowed = @change.unfollowed
+        end
+      end
+      
+      if @followed && @followed.count > 0
+        response = @twitter.get("/1.1/users/lookup.json?user_id=#{@followed.join(",")}")
+        data = JSON.parse(response.body)
+        
+        @followed_screen_names = data.map{ |x| x["screen_name"]}
+      end
+      
+      if @unfollowed && @unfollowed.count > 0
+        response = @twitter.get("/1.1/users/lookup.json?user_id=#{@unfollowed.join(",")}")
+        data = JSON.parse(response.body)
+        
+        @unfollowed_screen_names = data.map{ |x| x["screen_name"]}
+      end
+    end
+    
+    haml(:check)
   end
+  
+  private
+    def analyse_changes(s1, s2)
+      followed = []
+      unfollowed = []
+      diff = (s1^s2).to_a
+      diff.each do |change|
+        if s1.include?(change)
+          unfollowed << change
+        else
+          followed << change
+        end
+      end
+      {:followed => followed, :unfollowed => unfollowed}
+    end
 end
